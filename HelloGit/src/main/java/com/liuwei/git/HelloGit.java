40767368a7da91206700cab5387ea6bd3e960f10
liuwei <694667706@qq.com>
2020-06-01 21:18:49 +0800

    第二次提交

--- HelloGit/src/main/java/com/liuwei/git/HelloGit.java
+++ HelloGit/src/main/java/com/liuwei/git/HelloGit.java
@@ -7,5 +7,6 @@ package com.liuwei.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("version 2");
     }
 }
