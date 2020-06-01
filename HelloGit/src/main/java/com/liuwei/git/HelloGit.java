aaea87c5161f571be9fce177c4d50161531eed06
liuwei <694667706@qq.com>
2020-06-01 23:15:56 +0800

    创建dev分支

--- HelloGit/src/main/java/com/liuwei/git/HelloGit.java
+++ HelloGit/src/main/java/com/liuwei/git/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("version 2");
+        System.out.println("In dev Branch");
     }
 }
