6374fc17eb9fd449dfea65d7f8bd3f6afb980b09
liuwei <694667706@qq.com>
2020-06-01 21:13:04 +0800

    第一次提交

---
+++ HelloGit/src/main/java/com/liuwei/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.liuwei.git;
+
+/**
+ * @author liuwei
+ * @create 2020-06-01 18:05
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
